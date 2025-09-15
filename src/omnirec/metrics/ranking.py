85a7821e8c49d1fae1419a97618ad3465aae164b
Moritz Baumgart <baumgartmoritz@web.de>
2025-09-15 11:44:18 +0200

    fix: handle new prediction format in metric implementations

--- src/omnirec/metrics/ranking.py
+++ src/omnirec/metrics/ranking.py
@@ -13,6 +13,20 @@ class RankingMetric(Metric):
         else:
             self._k_list = [k]
 
+    def make_topk_dict(
+        self, predictions: DataFrame
+    ) -> dict[int, tuple[list[int], list[float]]]:
+        """
+        Convert predictions DataFrame with columns [user, item, rating, rank]
+        into {user: ([items sorted by rank], [scores])}.
+        """
+        topk = {}
+        for user, group in predictions.sort_values("rank").groupby("user"):
+            items = group["item"].to_list()
+            scores = group["rating"].to_list()
+            topk[user] = (items, scores)
+        return topk
+
 
 class NDCG(RankingMetric):
     # TODO: Docs, maybe add latex formula on how computed?
@@ -20,7 +34,8 @@ class NDCG(RankingMetric):
         super().__init__(k)
 
     def calculate(self, predictions: DataFrame, test: DataFrame) -> MetricResult:
-        # FIXME: Fix metric implementation, adapt to new data format
+        top_k_dict = self.make_topk_dict(predictions)
+
         discounted_gain_per_k = np.array(
             [1 / np.log2(i + 1) for i in range(1, max(self._k_list) + 1)]
         )
@@ -29,9 +44,9 @@ class NDCG(RankingMetric):
             for ind in range(len(discounted_gain_per_k))
         ]
         ndcg_per_user_per_k: dict[int, list] = {}
-        for user, (predictions, _) in top_k_dict.items():
+        for user, (pred, _) in top_k_dict.items():
             positive_test_interactions = test["item"][test["user"] == user].to_numpy()
-            hits = np.isin(predictions[: max(self._k_list)], positive_test_interactions)
+            hits = np.isin(pred[: max(self._k_list)], positive_test_interactions)
             user_dcg = np.where(hits, discounted_gain_per_k[: len(hits)], 0)
             for k in self._k_list:
                 user_ndcg = user_dcg[:k].sum() / ideal_discounted_gain_per_k[k - 1]
@@ -50,11 +65,13 @@ class HR(RankingMetric):
         super().__init__(k)
 
     def calculate(self, predictions: DataFrame, test: DataFrame) -> MetricResult:
+        top_k_dict = self.make_topk_dict(predictions)
+
         hr_per_user_per_k: dict[int, list] = {}
         # FIXME: Fix metric implementation, adapt to new data format
-        for user, (predictions, _) in top_k_dict.items():
+        for user, (pred, _) in top_k_dict.items():
             positive_test_interactions = test["item"][test["user"] == user].to_numpy()
-            hits = np.isin(predictions[: max(self._k_list)], positive_test_interactions)
+            hits = np.isin(pred[: max(self._k_list)], positive_test_interactions)
             for k in self._k_list:
                 user_hr = hits[:k].sum()
                 user_hr = 1 if user_hr > 0 else 0
@@ -69,16 +86,18 @@ class Recall(RankingMetric):
     def __init__(self, k: int | list[int]) -> None:
         super().__init__(k)
 
-    def calculate(self, predictions: DataFrame, test: DataFrame) -> list[float]:
-        # FIXME: Fix metric implementation, adapt to new data format
+    def calculate(self, predictions: DataFrame, test: DataFrame) -> MetricResult:
+        top_k_dict = self.make_topk_dict(predictions)
+
         recall_per_user_per_k: dict[int, list] = {}
-        for user, (predictions, _) in top_k_dict.items():
+        for user, (pred, _) in top_k_dict.items():
             positive_test_interactions = test["item"][test["user"] == user].to_numpy()
-            hits = np.isin(predictions[: max(self._k_list)], positive_test_interactions)
+            hits = np.isin(pred[: max(self._k_list)], positive_test_interactions)
             for k in self._k_list:
                 user_recall = hits[:k].sum() / min(len(positive_test_interactions), k)
                 recall_per_user_per_k.setdefault(k, []).append(user_recall)
         scores: list[float] = [
             float(sum(v)) / len(v) for v in recall_per_user_per_k.values()
         ]
-        return scores
+        scores_dict = {k: score for k, score in zip(self._k_list, scores)}
+        return MetricResult(__class__.__name__, scores_dict)
