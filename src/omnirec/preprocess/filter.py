5a88fa27ee378f7c816f1d544430dc3a795bd090
Moritz Baumgart <baumgartmoritz@web.de>
2025-10-22 15:36:32 +0200

    feat: add rating filter

--- src/omnirec/preprocess/filter.py
+++ src/omnirec/preprocess/filter.py
@@ -24,3 +24,22 @@ class TimeFilter(Preprocessor[RawData, RawData]):
             mask &= df["timestamp"] <= self._end.timestamp()
         df = df.loc[mask]
         return dataset.replace_data(RawData(df))
+
+
+class RatingFilter(Preprocessor[RawData, RawData]):
+    def __init__(
+        self, lower: Optional[int | float] = None, upper: Optional[int | float] = None
+    ) -> None:
+        super().__init__()
+        self._lower = lower
+        self._upper = upper
+
+    def process(self, dataset: RecSysDataSet[RawData]) -> RecSysDataSet[RawData]:
+        df = dataset._data.df
+        mask = pd.Series(True, index=df.index)
+        if self._lower is not None:
+            mask &= df["rating"] >= self._lower
+        if self._upper is not None:
+            mask &= df["rating"] <= self._upper
+        df = df.loc[mask]
+        return dataset.replace_data(RawData(df))
