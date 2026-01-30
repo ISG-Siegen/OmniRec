687024494f216fada58937f624b16fbf270e9830
Moritz Baumgart <baumgartmoritz@web.de>
2026-01-30 15:37:55 +0100

    feat: skip subprocess call to uv if venv already exist
    The call to uv can be forced with force=True

--- src/omnirec/runner/envs.py
+++ src/omnirec/runner/envs.py
@@ -34,8 +34,17 @@ class Env:
         else:
             self._path = get_data_dir() / "envs" / name
 
-    def create(self):
-        # TODO: Creating env shows up every time, this might be misleading
+    def create(self, force=False):
+        flag_file = self._path / ".omnirec"
+
+        if flag_file.exists() and force:
+            logger.info(
+                f"Env '{self._name}' already exists at {self._path}, but {force=}! Recreating..."
+            )
+        elif flag_file.exists() and not force:
+            logger.info(f"Reusing existing env '{self._name}' at {self._path}")
+            return
+
         logger.info(f"Creating env '{self._name}' at {self._path}")
         proc = self._run(
             ["uv", "venv", "-p", self._python_version, self._path.absolute()]
@@ -47,7 +56,7 @@ class Env:
             ["uv", "pip", "install", "-p", self.py_path.absolute(), *self._packages]
         )
         self._handle_proc(proc)
-
+        flag_file.touch()
         logger.info("Done.")
 
     @property
